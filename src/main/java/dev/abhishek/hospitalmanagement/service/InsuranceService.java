07b31c3b2dc5f6b17275fa2e391b47913df00fac
abhishek <aabhiojha08@outlook.com>
2026-01-27 16:10:52 +0545

    feat: lombok RequiredArgsConstructor usage

--- src/main/java/dev/abhishek/hospitalmanagement/service/InsuranceService.java
+++ src/main/java/dev/abhishek/hospitalmanagement/service/InsuranceService.java
@@ -7,18 +7,31 @@ import dev.abhishek.hospitalmanagement.repository.PatientRepository;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class InsuranceService {
 
+    //    @Autowired
     private final InsuranceRepository insuranceRepository;
+    //    @Autowired
     private final PatientRepository patientRepository;
 
+    // this quietly does constructor injection
+    // Required args constructor
+//    public InsuranceService(
+//            InsuranceRepository insuranceRepository,
+//            PatientRepository patientRepository
+//    ) {
+//        this.insuranceRepository = insuranceRepository;
+//        this.patientRepository = patientRepository;
+//    }
+
     @Transactional
-    public Patient assignInsuranceToPatient(Insurance insurance, Long patientId){
-        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new EntityNotFoundException("Patient not found with id: "+ patientId));
+    public Patient assignInsuranceToPatient(Insurance insurance, Long patientId) {
+        Patient patient = patientRepository.findById(patientId).orElseThrow(() -> new EntityNotFoundException("Patient not found with id: " + patientId));
 
         patient.setInsurance(insurance);
 
