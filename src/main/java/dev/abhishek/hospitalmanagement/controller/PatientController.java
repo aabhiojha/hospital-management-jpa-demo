ba13be6268c61129586ffb434adc8548233fb4cc
Abhishekojha <aojha6822@gmail.com>
2026-02-10 12:53:27 +0545

    feat: patient only crud

--- src/main/java/dev/abhishek/hospitalmanagement/controller/PatientController.java
+++ src/main/java/dev/abhishek/hospitalmanagement/controller/PatientController.java
@@ -5,36 +5,51 @@ import dev.abhishek.hospitalmanagement.exceptions.patient.PatientNotFoundExcepti
 import dev.abhishek.hospitalmanagement.repository.PatientRepository;
 import dev.abhishek.hospitalmanagement.service.PatientService;
 import lombok.RequiredArgsConstructor;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @RestController()
 @RequiredArgsConstructor
+@RequestMapping("/api/patients")
 public class PatientController {
 
     final PatientService patientService;
 //    PatientRepository patientRepository;
 
-    @GetMapping("/api/patients/{id}")
-    public Patient getPatientById(@PathVariable Long id){
-         return patientService.getPatientByID(id);
+    @GetMapping
+    public ResponseEntity<List<Patient>> getAllPatients(){
+        List<Patient> allPatients = patientService.getAllPatients();
+        return new ResponseEntity<>(allPatients, HttpStatus.OK);
     }
 
-    @GetMapping("/api/patients/{email}")
-    public Patient getPatientByEmail(@RequestParam String email){
-        return patientService.getPatientByEmail(email)
-                .orElseThrow(() -> new PatientNotFoundException(email));
+    @GetMapping("/{id}")
+    public ResponseEntity<Patient> getPatientById(@PathVariable Long id){
+         return new ResponseEntity<>(patientService.getPatientByID(id), HttpStatus.OK);
     }
 
+    @PostMapping
+    public ResponseEntity<?> createPatientEntry(@RequestBody Patient patient){
+        Patient patientEntry = patientService.createPatientEntry(patient);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
-//    @PostMapping("/api/patients/")
-//     public Patient createPatient(@RequestBody Patient patient) {
-//          return patientRepository.(id);
-//     }
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updatePatientEntry(@RequestBody Patient patient, @PathVariable long id){
+        Optional<Patient> updatedPatient = patientService.updatePatient(patient, id);
+        return new ResponseEntity<>(updatedPatient, HttpStatus.OK);
+    }
 
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deletePatientEntry(@PathVariable long id){
+        patientService.deletePatient(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 
 
--- src/main/java/dev/abhishek/hospitalmanagement/service/PatientService.java
+++ src/main/java/dev/abhishek/hospitalmanagement/service/PatientService.java
@@ -5,7 +5,9 @@ import dev.abhishek.hospitalmanagement.exceptions.patient.PatientNotFoundExcepti
 import dev.abhishek.hospitalmanagement.repository.PatientRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -14,16 +16,40 @@ public class PatientService {
 
     private final PatientRepository patientRepository;
 
+    public List<Patient> getAllPatients() {
+        return patientRepository.findAll();
+    }
+
     public Patient getPatientByID(long id) {
         return patientRepository.findById(id)
-                .orElseThrow(()-> new PatientNotFoundException(id));
+                .orElseThrow(() -> new PatientNotFoundException(id));
     }
 
-    public Optional<Patient> getPatientByEmail(String email){
-        return patientRepository.findByEmail(email);
+    @Transactional
+    public Patient createPatientEntry(Patient patient) {
+        return patientRepository.save(patient);
     }
 
-//    public Optional<Patient> getPatientByID
+    @Transactional
+    public Optional<Patient> updatePatient(Patient patient, Long id) {
+        // check if the patient really exists by id
+        Optional<Patient> patientFromDb = patientRepository.findById(id);
+        if (patientFromDb.isEmpty()) {
+            return Optional.empty();
+        }
+        Patient existingPatient = patientFromDb.get();
+        existingPatient.setName(patient.getName());
+        existingPatient.setBirthDate(patient.getBirthDate());
+        existingPatient.setEmail(patient.getEmail());
+        existingPatient.setGender(patient.getGender());
+        patientRepository.save(existingPatient);
+        return patientFromDb;
+    }
 
+
+    @Transactional
+    public void deletePatient(long id){
+        patientRepository.deleteById(id);
+    }
 
 }
